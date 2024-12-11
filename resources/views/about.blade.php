385a0923a6362732444495343609e050626df306
DuskEVE <x987000@gmail.com>
2024-12-11 17:26:40 +0800

    initial commit, complete basic component and 3 page layout

---
+++ resources/views/about.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot:title>
+        About page
+    </x-slot:title>
+    <h1>Hello about!</h1>
+</x-layout>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot:title>
+        Home page
+    </x-slot:title>
+    <h1>Hello home!</h1>
+</x-layout>
