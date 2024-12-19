6b9c48cefb0a442ff865a5181eb17e618f22be5a
DuskEVE <x987000@gmail.com>
2024-12-19 17:35:14 +0800

    create JobFactory

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,38 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 use PhpParser\Node\Expr\Cast\Array_;
 
 class Job extends Model{
+    use HasFactory;
+
     protected $table = 'job_listings';
 
     protected $fillable = ['title', 'salary'];
 }
-
-// class Job{
-
-//     public static function all(){
-//         $jobs = [
-//             ['id'=>1, 'title'=>'gamer', 'salary'=>0], 
-//             ['id'=>2, 'title'=>'programmer', 'salary'=>50000], 
-//             ['id'=>3, 'title'=>'worker', 'salary'=>28000]
-//         ];
-
-//         return $jobs;
-//     }
-
-//     public static function find(int $id): array{
-//         $jobs = [
-//             ['id'=>1, 'title'=>'gamer', 'salary'=>0], 
-//             ['id'=>2, 'title'=>'programmer', 'salary'=>50000], 
-//             ['id'=>3, 'title'=>'worker', 'salary'=>28000]
-//         ];
-//         $target = Arr::first($jobs, function($job) use ($id){
-//             return $job['id'] == $id;
-//         });
-
-//         return $target;
-//     }
-// }
