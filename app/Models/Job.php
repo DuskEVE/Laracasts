d24e46ff4e5c1dcca42aebd6523aff19392db028
DuskEVE <x987000@gmail.com>
2024-12-20 20:10:42 +0800

    greate tags migration, create relation between job_listing table and tag table

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,8 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Arr;
-use PhpParser\Node\Expr\Cast\Array_;
+
 
 class Job extends Model{
     use HasFactory;
@@ -13,4 +12,12 @@ class Job extends Model{
     protected $table = 'job_listings';
 
     protected $fillable = ['title', 'salary'];
+
+    public function employer(){
+        return $this->belongsTo(Employer::class);
+    }
+
+    public function tags(){
+        return $this->belongsToMany(Tag::class, foreignPivotKey:'job_listing_id');
+    }
 }
